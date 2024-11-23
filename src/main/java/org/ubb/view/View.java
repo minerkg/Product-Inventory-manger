0cc8816c9a5d74d989effd32a113dae973167d0c
Csiszer <csiszerors@gmail.com>
2024-11-23 13:12:55 +0200

    params for enum v0.2

--- src/main/java/org/ubb/view/View.java
+++ src/main/java/org/ubb/view/View.java
@@ -1,5 +1,6 @@
 package org.ubb.view;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -21,9 +22,13 @@ public class View {
 
         String commandAndParams = scanner.nextLine();
         System.out.println(commandAndParams);
+
         String command = commandAndParams.split("\\s+")[0];
-        List<String> parameters = Arrays.asList(commandAndParams.split("\\s+"));
-        parameters.remove(0);
+        List<String> parameters = new ArrayList<>();
+
+        String[] params = commandAndParams.split("\\s+");
+        for (int i=1; i < params.length; i++)
+            parameters.add(params[i]);
 
         ViewMenuItems selectedItem = ViewMenuItems.valueOf(command.toUpperCase());
         selectedItem.setParams(parameters);
