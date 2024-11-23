e727fec71a55ef9bab77e8c20376625373306f6f
Csiszer <csiszerors@gmail.com>
2024-11-23 10:32:29 +0200

    product class is ready

---
+++ src/main/java/org/ubb/Main.java
@@ -0,0 +1,7 @@
+package org.ubb;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/ubb/domain/Product.java
@@ -0,0 +1,39 @@
+package org.ubb.domain;
+
+public class Product {
+
+
+    private Long id;
+    private String brand;
+    private String availability;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(String availability) {
+        this.availability = availability;
+    }
+
+    public Product(Long id, String brand, String availability) {
+        this.id = id;
+        this.brand = brand;
+        this.availability = availability;
+    }
+}
